5f2208e278e67c42ec4b365334c877e47d5a3cc6
Piet Laur <piet.laur@gmail.com>
2019-10-12 22:46:31 +0200

    removed submodule

---
+++ src/Display.cpp
@@ -0,0 +1,20 @@
+/*
+ * Display.cpp
+ *
+ *  Created on: Oct 12, 2019
+ *      Author: Piet
+ */
+
+#include "Display.hpp"
+
+Display::Display(LTDC_HandleTypeDef *hltdc)
+: handle(hltdc)
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Display::~Display() {
+	// TODO Auto-generated destructor stub
+}
+
